0e68f335e89a91966b6a623cb656c383318bdb59
kdu2030 <kdu2030@gmail.com>
2024-06-15 10:31:09 -0400

    Init Commit and Start On navbar

---
+++ src/scraper/elder_law_answers_scraper.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import Playwright, Browser
+
+
+class ElderLawAnswersScraper:
+    ELDER_LAW_ANSWERS_BASE_URL = "https://attorney.elderlawanswers.com"
+
+    def __init__(self, playwright: Playwright, username: str, password: str):
+        self.browser: Browser = playwright.chromium.launch(headless=False)
+        self.username = username
+        self.password = password
+
+    def sign_in(self):
+        page = self.browser.new_page()
+        page.goto(f"{self.ELDER_LAW_ANSWERS_BASE_URL}/login")
+
+        username_input = page.query_selector("input#id")
+        username_input.fill(self.username)
+
+        password_input = page.query_selector("input#password")
+        password_input.fill(self.password)
+
+        button_input = page.query_selector("button[type='submit']")
+        button_input.click()
+        # TODO: For testing purposes only, remove
+        page.wait_for_event("close", timeout=0)
