b0fc46b683829e5bbb648fdd0082b5f7c9730177
kdu2030 <kdu2030@gmail.com>
2024-06-19 13:53:31 -0400

    Fix settings import error

--- src/herndon_law_seo_helper/herndon_law_seo_helper/wsgi.py
+++ src/herndon_law_seo_helper/herndon_law_seo_helper/wsgi.py
@@ -11,10 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-from django.conf import Settings
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'herndon_law_seo_helper.production_settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=Settings.STATIC_ROOT)
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
