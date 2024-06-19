645a4449d602d46dbc634212c30477366e2d0359
kdu2030 <kdu2030@gmail.com>
2024-06-19 12:16:44 -0400

    Remove SSL Redirection

--- src/herndon_law_seo_helper/herndon_law_seo_helper/production_settings.py
+++ src/herndon_law_seo_helper/herndon_law_seo_helper/production_settings.py
@@ -118,3 +118,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = False
