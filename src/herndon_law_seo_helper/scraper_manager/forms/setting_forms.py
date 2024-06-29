58e026bed78d67d947a4052514aaab622055d158
kdu2030 <kdu2030@gmail.com>
2024-06-28 20:35:00 -0400

    Add Email Form

--- src/herndon_law_seo_helper/scraper_manager/forms/setting_forms.py
+++ src/herndon_law_seo_helper/scraper_manager/forms/setting_forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email", "class": "form-control d-none"}),
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email", "class": "form-control"}),
                              label="Email", required=True)
--- src/herndon_law_seo_helper/scraper_manager/views/settings.py
+++ src/herndon_law_seo_helper/scraper_manager/views/settings.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from ..forms.setting_forms import EmailForm
 
 
 def settings_get(response: HttpRequest) -> HttpResponse:
-    return render(response, "scraper_manager/settings.html")
+    email_form = EmailForm()
+    return render(response, "scraper_manager/settings.html", {"email_form": email_form})
