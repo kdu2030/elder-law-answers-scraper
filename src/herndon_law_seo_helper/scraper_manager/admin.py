e881fb67c7dcdd7fb5c603ccc53d464b47fbbc70
kdu2030 <kdu2030@gmail.com>
2024-06-30 17:56:09 -0400

    Add Blog Post Model

--- src/herndon_law_seo_helper/scraper_manager/admin.py
+++ src/herndon_law_seo_helper/scraper_manager/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from scraper_manager.models.setting_models import SourceConfiguration
+from scraper_manager.models.blog_posts import BlogPost
 
 # Register your models here.
 
 admin.site.register(SourceConfiguration)
+admin.site.register(BlogPost)
