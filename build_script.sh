84a074c724b539a8022181c01dc4f4405521da6c
kdu2030 <kdu2030@gmail.com>
2024-07-01 19:35:38 -0400

    Update Build Script

--- build_script.sh
+++ build_script.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-
-pip install -r requirements.txt
 playwright install
 
 cd ./src/herndon_law_seo_helper
-python3 manage.py migrate
+
+python3 manage.py migrate --settings herndon_law_seo_helper.production_settings
+python3 manage.py collectstatic
+
+gunicorn herndon_law_seo_helper.wsgi
