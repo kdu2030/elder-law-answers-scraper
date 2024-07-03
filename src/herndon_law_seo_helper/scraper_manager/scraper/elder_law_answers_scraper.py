a87b4406a08dc89f1ead2f48a89c3dd19fb8b2fa
kdu2030 <kdu2030@gmail.com>
2024-07-03 07:22:45 -0400

    Turn on headless

--- src/herndon_law_seo_helper/scraper_manager/scraper/elder_law_answers_scraper.py
+++ src/herndon_law_seo_helper/scraper_manager/scraper/elder_law_answers_scraper.py
@@ -19,7 +19,7 @@ class ElderLawAnswersScraper:
     MAX_ARTICLE_PAGES = 20
 
     def __init__(self, playwright: Playwright, username: str, password: str):
-        self.browser: Browser = playwright.firefox.launch(headless=False)
+        self.browser: Browser = playwright.firefox.launch(headless=True)
         self.username = username
         self.password = password
 
