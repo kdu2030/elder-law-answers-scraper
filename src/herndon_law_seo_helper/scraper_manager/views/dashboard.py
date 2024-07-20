4034b538c5f378dcf47feda6ba18c8c922649ccd
kdu2030 <kdu2030@gmail.com>
2024-07-20 11:10:39 -0400

    Get Month Start Dates

--- src/herndon_law_seo_helper/scraper_manager/views/dashboard.py
+++ src/herndon_law_seo_helper/scraper_manager/views/dashboard.py
@@ -6,6 +6,19 @@ from dateutil.relativedelta import relativedelta
 from ..scraper.elder_law_answers_scraper import ScraperErrorCode
 
 
+def get_chart_data(current_month: int, current_year: int):
+    current_month_start = datetime(
+        year=current_year, month=current_month, day=1)
+
+    month_start_dates = [current_month_start]
+    months_to_add = 4
+
+    for i in range(0, months_to_add):
+        new_month = current_month_start - relativedelta(months=(i + 1))
+        month_start_dates.insert(0, new_month)
+    print(month_start_dates)
+
+
 def format_status_info(error_code: ScraperErrorCode):
     if error_code == ScraperErrorCode.LOGIN_FAILED.value:
         return "Website login failed"
@@ -16,11 +29,7 @@ def format_status_info(error_code: ScraperErrorCode):
     return "An unknown error occured"
 
 
-def get_blog_post_attempts():
-    current_date = datetime.now()
-    current_month = current_date.month
-    current_year = current_date.year
-
+def get_blog_post_attempts(current_month: int, current_year: int):
     start_date = datetime(year=current_year, month=current_month, day=1)
     end_date = start_date + relativedelta(months=1)
 
@@ -40,15 +49,12 @@ def get_blog_post_attempts():
             "status_info": successful_post.post_title
         })
 
-    # TODO: Handle Failure Reason Formatting
     for failure_post in failure_posts:
         attempt_table_data.append({
             "date_attempted": failure_post.date_attempted,
             "status": "Warning" if failure_post.error_code == ScraperErrorCode.UNABLE_TO_FIND_ARTICLE.value else "Failure",
             "status_info": format_status_info(failure_post.error_code)
         })
-
-    # TODO: Sort by Date attempted
 
     attempt_table_data.sort(
         key=lambda row: row["date_attempted"], reverse=True)
@@ -57,5 +63,10 @@ def get_blog_post_attempts():
 
 
 def dashboard_get(request: HttpRequest) -> HttpResponse:
-    post_attempt_rows = get_blog_post_attempts()
+    current_date = datetime.now()
+    current_month = current_date.month
+    current_year = current_date.year
+
+    post_attempt_rows = get_blog_post_attempts(current_month, current_year)
+    get_chart_data(current_month, current_year)
     return render(request, "scraper_manager/dashboard.html", {"attempt_rows": post_attempt_rows})
