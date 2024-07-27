0569bad1c5ca92ae07f58602c44d6b16cd2194c5
kdu2030 <kdu2030@gmail.com>
2024-07-26 20:17:57 -0400

    Fix Checkboxes to Update Correctly Based on User Permissions

--- src/herndon_law_seo_helper/scraper_manager/views/admin.py
+++ src/herndon_law_seo_helper/scraper_manager/views/admin.py
@@ -10,7 +10,9 @@ def get_table_row_for_user(user: User) -> Dict[str, str]:
     username = user.username
     profile_picture = UserProfilePicture.objects.filter(user=user).first()
     image_src = "https://i.ibb.co/y4KL53m/Default-Profile-Picture-Transparent.png"
-    user_permission_codes = UserPermissionCode.objects.filter(user=user) or []
+    user_permissions = UserPermissionCode.objects.filter(user=user) or []
+    user_permission_codes = list(map(
+        lambda permission: permission.permission_code, user_permissions))
 
     can_view_admin = PermissionCode.VIEW_ADMIN.value in user_permission_codes
     can_edit_config = PermissionCode.EDIT_WEBSITE_CONFIG.value in user_permission_codes
