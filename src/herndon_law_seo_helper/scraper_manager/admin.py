647bc387a47aef4dd62bdf2d15d1d339ad366f2a
kdu2030 <kdu2030@gmail.com>
2024-07-26 20:03:15 -0400

    Add Model for User Permissions and Add User Permissions Checkboxes

--- src/herndon_law_seo_helper/scraper_manager/admin.py
+++ src/herndon_law_seo_helper/scraper_manager/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from scraper_manager.models.setting_models import SourceConfiguration, WebsiteConfiguration
 from scraper_manager.models.blog_posts import BlogPost, PostFailureLog
-from scraper_manager.models.user_models import UserProfilePicture
+from scraper_manager.models.user_models import UserProfilePicture, UserPermissionCode
 
 # Register your models here.
 
@@ -10,3 +10,4 @@ admin.site.register(BlogPost)
 admin.site.register(WebsiteConfiguration)
 admin.site.register(UserProfilePicture)
 admin.site.register(PostFailureLog)
+admin.site.register(UserPermissionCode)
--- src/herndon_law_seo_helper/scraper_manager/models/user_models.py
+++ src/herndon_law_seo_helper/scraper_manager/models/user_models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from enum import Enum
+
+
+class PermissionCode(Enum):
+    VIEW_ADMIN = 1
+    EDIT_WEBSITE_CONFIG = 2
+
+
+class UserPermissionCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    permission_code = models.IntegerField()
 
 
 class UserProfilePicture(models.Model):
