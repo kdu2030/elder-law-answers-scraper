65a2e44373c7338980b500773492452822f66f7d
kdu2030 <kdu2030@gmail.com>
2024-07-28 14:12:28 -0400

    Add Custom 404 Page

--- src/herndon_law_seo_helper/scraper_manager/views/page_not_found.py
+++ src/herndon_law_seo_helper/scraper_manager/views/page_not_found.py
@@ -1,6 +1,6 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render
 
 
-def page_not_found_get(request: HttpRequest) -> HttpResponse:
+def page_not_found_get(request: HttpRequest, exception: Http404) -> HttpResponse:
     return render(request, "scraper_manager/page-not-found.html")
